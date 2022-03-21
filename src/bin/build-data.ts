ab9516d906cfccd2839d15be0128d563f3ab4b87
garronej <joseph.garrone.gj@gmail.com>
2022-03-21 14:17:50 +0100

    :bug:  Fix the bugs at launch

--- src/bin/build-data.ts
+++ src/bin/build-data.ts
@@ -10,55 +10,60 @@ export const serviceJsonRelativeFilePath = "service.json";
 
 export const compiledDataJsonRelativeFilePath = "compiledData.json";
 
-(async () => {
-    const projectDirPath = pathJoin(__dirname, "..", "..");
-    const dataDirPath = pathJoin(projectDirPath, "data");
-    const buildDirPath = pathJoin(dataDirPath, "build");
-    const compiledDataWithoutReferentJsonRelativeFilePath =
-        "compiledData_withoutReferents.json";
+if (require.main === module) {
+    (async () => {
+        const projectDirPath = pathJoin(__dirname, "..", "..");
+        const dataDirPath = pathJoin(projectDirPath, "data");
+        const buildDirPath = pathJoin(dataDirPath, "build");
+        const compiledDataWithoutReferentJsonRelativeFilePath =
+            "compiledData_withoutReferents.json";
 
-    const { compiledData } = await (async () => {
-        const read = (relativeFilePath: string) =>
-            JSON.parse(
-                fs
-                    .readFileSync(pathJoin(dataDirPath, relativeFilePath))
-                    .toString("utf8"),
+        const { compiledData } = await (async () => {
+            const read = (relativeFilePath: string) =>
+                JSON.parse(
+                    fs
+                        .readFileSync(pathJoin(dataDirPath, relativeFilePath))
+                        .toString("utf8"),
+                );
+
+            const { catalog } = await buildCatalog({
+                "softwareRows": read(softwareJsonRelativeFilePath),
+                "referentRows": read(referentJsonRelativeFilePath),
+                "softwareReferentRows": read(
+                    softwareReferentJsonRelativeFilePath,
+                ),
+                "log": console.log,
+            });
+
+            const compiledData: CompiledData<"with referents"> = {
+                catalog,
+                "services": read(serviceJsonRelativeFilePath),
+            };
+
+            return { compiledData };
+        })();
+
+        const compiledData_withoutReferents: CompiledData<"without referents"> =
+            {
+                ...compiledData,
+                "catalog": compiledData.catalog.map(removeReferent),
+            };
+
+        if (!fs.existsSync(buildDirPath)) {
+            fs.mkdirSync(buildDirPath);
+        }
+
+        for (const [relativeJsonFilePath, data] of [
+            [compiledDataJsonRelativeFilePath, compiledData],
+            [
+                compiledDataWithoutReferentJsonRelativeFilePath,
+                compiledData_withoutReferents,
+            ],
+        ] as const) {
+            fs.writeFileSync(
+                pathJoin(buildDirPath, relativeJsonFilePath),
+                Buffer.from(JSON.stringify(data, null, 2), "utf8"),
             );
-
-        const { catalog } = await buildCatalog({
-            "softwareRows": read(softwareJsonRelativeFilePath),
-            "referentRows": read(referentJsonRelativeFilePath),
-            "softwareReferentRows": read(softwareReferentJsonRelativeFilePath),
-            "log": console.log,
-        });
-
-        const compiledData: CompiledData<"with referents"> = {
-            catalog,
-            "services": read(serviceJsonRelativeFilePath),
-        };
-
-        return { compiledData };
+        }
     })();
-
-    const compiledData_withoutReferents: CompiledData<"without referents"> = {
-        ...compiledData,
-        "catalog": compiledData.catalog.map(removeReferent),
-    };
-
-    if (!fs.existsSync(buildDirPath)) {
-        fs.mkdirSync(buildDirPath);
-    }
-
-    for (const [relativeJsonFilePath, data] of [
-        [compiledDataJsonRelativeFilePath, compiledData],
-        [
-            compiledDataWithoutReferentJsonRelativeFilePath,
-            compiledData_withoutReferents,
-        ],
-    ] as const) {
-        fs.writeFileSync(
-            pathJoin(buildDirPath, relativeJsonFilePath),
-            Buffer.from(JSON.stringify(data, null, 2), "utf8"),
-        );
-    }
-})();
+}
--- src/tools/git.ts
+++ src/tools/git.ts
@@ -1,75 +1,90 @@
 import { exec } from "./exec";
+import * as runExclusive from "run-exclusive";
 
-export async function git(params: {
-    owner: string;
-    repo: string;
-    shaish?: string;
-    commitAuthorEmail?: string;
-    github_token: string;
-    action: () => Promise<
-        | { doCommit: false }
-        | { doCommit: true; doAddAll: boolean; message: string }
-    >;
-}) {
-    const {
-        owner,
-        repo,
-        shaish,
-        commitAuthorEmail = "actions@github.com",
-        action,
-        github_token,
-    } = params;
+const tmpDirPrefix = "git_tmp_xIdLsIt";
 
-    await exec(
-        `git clone ${
-            shaish === undefined ? "--depth 1 " : " "
-        }https://${github_token}@github.com/${owner}/${repo}`,
-    );
+//NOTE: It changes the working directory so we can't have multiple instance
+//running simultaneously.
+export const git = runExclusive.build(
+    async (params: {
+        owner: string;
+        repo: string;
+        shaish?: string;
+        commitAuthorEmail?: string;
+        github_token: string;
+        action: () => Promise<
+            | { doCommit: false }
+            | { doCommit: true; doAddAll: boolean; message: string }
+        >;
+    }) => {
+        const {
+            owner,
+            repo,
+            shaish,
+            commitAuthorEmail = "actions@github.com",
+            action,
+            github_token,
+        } = params;
 
-    const cwd = process.cwd();
+        const tmpDir = `${tmpDirPrefix}_${repo}`;
 
-    process.chdir(repo);
+        await exec(`rm -rf ${tmpDir}`);
 
-    if (shaish !== undefined) {
-        await exec(`git checkout ${shaish}`);
-    }
-
-    const changesResult = await (async () => {
-        try {
-            return await action();
-        } catch (error) {
-            return error as Error;
-        }
-    })();
-
-    if (!(changesResult instanceof Error) && changesResult.doCommit) {
-        await exec(`git config --local user.email "${commitAuthorEmail}"`);
         await exec(
-            `git config --local user.name "${commitAuthorEmail.split("@")[0]}"`,
+            `git clone ${
+                shaish === undefined ? "--depth 1 " : " "
+            }https://${github_token}@github.com/${owner}/${repo} ${tmpDir}`,
         );
 
-        if (changesResult.doAddAll) {
-            await exec(`git add -A`);
+        const cwd = process.cwd();
+
+        process.chdir(tmpDir);
+
+        if (shaish !== undefined) {
+            await exec(`git checkout ${shaish}`);
         }
 
-        await exec(`git commit -am "${changesResult.message}"`);
-
-        {
-            const url = `https://${owner}:${github_token}@github.com/${owner}/${repo}.git`;
-
+        const changesResult = await (async () => {
             try {
-                await exec(`git push "${url}"`);
-            } catch {
-                await exec(`git push --set-upstream origin ${shaish} "${url}"`);
+                return await action();
+            } catch (error) {
+                return error as Error;
+            }
+        })();
+
+        if (!(changesResult instanceof Error) && changesResult.doCommit) {
+            await exec(`git config --local user.email "${commitAuthorEmail}"`);
+            await exec(
+                `git config --local user.name "${
+                    commitAuthorEmail.split("@")[0]
+                }"`,
+            );
+
+            if (changesResult.doAddAll) {
+                await exec(`git add -A`);
+            }
+
+            await exec(`git commit -am "${changesResult.message}"`);
+
+            {
+                const url = `https://${owner}:${github_token}@github.com/${owner}/${repo}.git`;
+
+                try {
+                    await exec(`git push "${url}"`);
+                } catch {
+                    await exec(
+                        `git push --set-upstream origin ${shaish} "${url}"`,
+                    );
+                }
             }
         }
-    }
 
-    process.chdir(cwd);
+        process.chdir(cwd);
 
-    await exec(`rm -r ${repo}`);
+        await exec(`rm -r ${tmpDir}`);
 
-    if (changesResult instanceof Error) {
-        throw changesResult;
-    }
-}
+        if (changesResult instanceof Error) {
+            throw changesResult;
+        }
+    },
+);
