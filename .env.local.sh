f2c071723c2208a8b7604ac5dfe473e99068a4f6
garronej <joseph.garrone.gj@gmail.com>
2022-03-21 15:35:55 +0100

    :rewind:  Restoring .env.local.sh as it was

--- .env.local.sh
+++ .env.local.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
 export CONFIGURATION=$(cat << EOF
-{ "keycloakParams": { "url": "https://etalab-auth.lab.sspcloud.fr/auth", "realm": "etalab", "clientId": "sill" }, "jwtClaims": { "email": "email", "familyName": "family_name", "firstName": "given_name", "username": "preferred_username", "groups": "groups", "locale": "locale" }, "dataRepoUrl": "https://github.com/etalab/sill-data", "buildBranch": "build", "githubPersonalAccessToken": { /* This env need to be defined in your .bashrc */ "envName": "GITHUB_PERSONAL_ACCESS_TOKEN" }, "port": 8080 }
+{
+	"keycloakParams": {
+		"url": "https://etalab-auth.lab.sspcloud.fr/auth",
+		"realm": "etalab",
+		"clientId": "sill"
+	},
+	"jwtClaims": {
+		"email": "email",
+		"familyName": "family_name",
+		"firstName": "given_name",
+		"username": "preferred_username",
+		"groups": "groups",
+		"locale": "locale"
+	},
+    "dataRepoUrl": "https://github.com/etalab/sill-data",
+    "buildBranch": "build",
+	"githubPersonalAccessToken": { 
+		/* This env need to be defined in your .bashrc */
+		"envName": "GITHUB_PERSONAL_ACCESS_TOKEN" 
+	},
+	"port": 8080
+}
 EOF
 ) 
 
