108469cc8bc4f8b502e9aaf5bb9a1b9adbf069e0
garronej <joseph.garrone.gj@gmail.com>
2022-04-26 13:14:43 +0200

    :sparkles:  Write a script to reset sill-data-test on sill-data

---
+++ src/bin/reset-data-test.ts
@@ -0,0 +1,17 @@
+import { execSync } from "child_process";
+
+// Script to run that will make etalab/sill-data-test is the same as etalab/sill-data
+
+const tmpDirBasename = "tmp_x3dL4bZdj4dbq2Td";
+
+execSync(
+    [
+        `rm -rf ${tmpDirBasename}`,
+        `git clone https://github.com/etalab/sill-data ${tmpDirBasename}`,
+        `cd ${tmpDirBasename}`,
+        `git remote set-url origin https://github.com/etalab/sill-data-test`,
+        `git push -f`,
+        `cd ..`,
+        `rm -rf ${tmpDirBasename}`,
+    ].join(" && "),
+);
