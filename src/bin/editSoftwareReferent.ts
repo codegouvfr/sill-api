80685ebdaeb4c37fc10d0890fd0298b9b0984a16
garronej <joseph.garrone.gj@gmail.com>
2022-05-10 08:59:36 +0200

    :construction_worker:  Add eddit script

---
+++ src/bin/editSoftwareReferent.ts
@@ -0,0 +1,78 @@
+import type { SoftwareReferentRow } from "../model/types";
+import { z } from "zod";
+import * as fs from "fs";
+import { join as pathJoin } from "path";
+import { assert } from "tsafe/assert";
+import type { Equals } from "tsafe";
+
+/*
+
+This script is meant to help edit and make sure it is well formatted sill-data/software.json
+
+cd ~/github/sill-api && npx tsc -w
+cd ~/github/sill-data 
+node ../sill-api/dist/bin/editSoftwareReferent.js
+
+*/
+
+const softwareReferentFilePath = pathJoin(
+    process.cwd(),
+    "softwareReferent.json",
+);
+
+const zSoftwareReferentRow = z.object({
+    "softwareId": z.number(),
+    "referentEmail": z.string(),
+    "isExpert": z.boolean(),
+    "useCaseDescription": z.string(),
+});
+
+type Got = ReturnType<typeof zSoftwareReferentRow["parse"]>;
+type Expected = SoftwareReferentRow;
+
+assert<Equals<Got, Expected>>();
+
+fs.writeFileSync(
+    softwareReferentFilePath,
+    Buffer.from(
+        JSON.stringify(
+            JSON.parse(
+                fs.readFileSync(softwareReferentFilePath).toString("utf8"),
+            ).map((softwareReferentRow: SoftwareReferentRow) => {
+                try {
+                    zSoftwareReferentRow.parse(softwareReferentRow);
+                } catch (exception) {
+                    console.log(softwareReferentRow);
+
+                    throw exception;
+                }
+
+                const {
+                    softwareId,
+                    referentEmail,
+                    isExpert,
+                    useCaseDescription,
+                    ...rest
+                } = softwareReferentRow;
+
+                try {
+                    assert(Object.keys(rest).length === 0);
+                } catch (error) {
+                    console.log(rest);
+
+                    throw error;
+                }
+
+                return {
+                    softwareId,
+                    referentEmail,
+                    isExpert,
+                    useCaseDescription,
+                };
+            }),
+            null,
+            2,
+        ),
+        "utf8",
+    ),
+);
