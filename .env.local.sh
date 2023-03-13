a9ea7f68f34c8ef4c7d2c86167a0407da14a24ce
garronej <joseph.garrone@data.gouv.fr>
2023-03-13 01:12:47 +0100

    Update env.local.sh

--- .env.local.sh
+++ .env.local.sh
@@ -7,7 +7,7 @@ export CONFIGURATION=$(cat << EOF
     "realm": "etalab",
     "clientId": "sill",
     "termsOfServices": "https://sill.code.gouv.fr/tos_fr.md",
-    "adminPassword": "$KEYCLOAK_ETALAB_ADMIN_PASSWORD",
+    "adminPassword": "$KEYCLOAK_CODEGOUV_ADMIN_PASSWORD",
     "agencyNameAttributeName:": "agencyName"
   },
   "jwtClaimByUserKey": {
@@ -16,11 +16,11 @@ export CONFIGURATION=$(cat << EOF
     "agencyName": "agency_name",
     "locale": "locale"
   },
-  "dataRepoSshUrl": "git@github.com:etalab/sill-data-test.git",
+  "dataRepoSshUrl": "git@github.com:codegouv/sill-data-test.git",
   "sshPrivateKeyForGitName": "$SSH_PRIVATE_KEY_FOR_GIT_NAME",
-  "sshPrivateKeyForGit": "$SSH_PRIVATE_KEY_FOR_GIT" 
+  "sshPrivateKeyForGit": "$SSH_PRIVATE_KEY_FOR_GIT",
+  "githubPersonalAccessToken": "$GITHUB_TOKEN"
 }
 EOF
 ) 
-
 $@
