aa1cab4f483d891699b44f0e6f06e2b2314544a1
garronej <joseph.garrone@data.gouv.fr>
2023-03-28 13:36:48 +0200

    Add user agent header

--- src/core/adapters/getWikidataSoftwareOptions.ts
+++ src/core/adapters/getWikidataSoftwareOptions.ts
@@ -13,7 +13,12 @@ export const getWikidataSoftwareOptions: GetWikidataSoftwareOptions = async ({ q
             "https://www.wikidata.org/w/api.php?action=wbsearchentities&format=json",
             `search=${encodeURIComponent(queryString)}`,
             `language=${language}`
-        ].join("&")
+        ].join("&"),
+        {
+            "headers": {
+                "User-Agent": "Socle interministériel de logiciels libres - Ap"
+            }
+        }
     ).then(response => response.json())) as any;
 
     return results.search.map(entry => ({
