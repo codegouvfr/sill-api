bd4fcd2abbfbac0e0e40079dbcf353617b6f36a7
garronej <joseph.garrone@data.gouv.fr>
2023-08-09 16:32:02 +0200

    Script optimized by chat gpt

--- src/scripts/reset-data-test.ts
+++ src/scripts/reset-data-test.ts
@@ -1,28 +1,37 @@
 import { execSync } from "child_process";
-import { compiledDataBranch } from "../core/adapters/dbApi";
-
-// Script to run that will make codegouvfr/sill-data-test is the same as codegouvfr/sill-data
 
 const tmpDirBasename = "tmp_x3dL4bZdj4dbq2Td";
-
 const repoUrl = "https://github.com/codegouvfr/sill-data";
 
-execSync(
-    [
-        `rm -rf ${tmpDirBasename}`,
-        `git clone --depth 1 ${repoUrl} ${tmpDirBasename}`,
-        `cd ${tmpDirBasename}`,
-        `git remote set-url origin ${repoUrl}-test`,
-        `git push -f`,
-        `cd ..`,
-        `rm -rf ${tmpDirBasename}`,
-        `git clone --branch ${compiledDataBranch} --depth 1 ${repoUrl} ${tmpDirBasename}`,
-        `cd ${tmpDirBasename}`,
-        `git remote set-url origin ${repoUrl}-test`,
-        `git push -f`,
-        `cd ..`,
-        `rm -rf ${tmpDirBasename}`
-    ].join(" && sleep 1 && ")
-);
+function executeCommand(command: string) {
+    console.log(`Executing: ${command}`);
+    execSync(command, { stdio: "inherit" });
+}
 
-console.log("https://github.com/codegouvfr/sill-data-test");
+try {
+    // Remove the temporary directory if it exists
+    executeCommand(`rm -rf ${tmpDirBasename}`);
+
+    // Clone the entire repo (including all branches and commit history)
+    executeCommand(`git clone ${repoUrl} ${tmpDirBasename}`);
+
+    // Change directory
+    process.chdir(tmpDirBasename);
+
+    // Fetch all branches from origin
+    executeCommand(`git fetch origin`);
+
+    // Change remote URL
+    executeCommand(`git remote set-url origin ${repoUrl}-test`);
+
+    // Push all branches to the new origin
+    executeCommand(`git push -f --all`);
+
+    // Change directory back
+    process.chdir("..");
+
+    // Remove the temporary directory
+    executeCommand(`rm -rf ${tmpDirBasename}`);
+} catch (error) {
+    console.error("An error occurred:", String(error));
+}
