458b04e9ed3530df65e6b342e133ec87df674c38
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2023-11-29 16:58:35 +0100

    add serviceProviders in selector

--- src/core/usecases/readWriteSillData/selectors.ts
+++ src/core/usecases/readWriteSillData/selectors.ts
@@ -143,7 +143,7 @@ const similarSoftwarePartition = createSelector(compiledData, (compiledData): So
 const softwares = createSelector(compiledData, similarSoftwarePartition, (compiledData, similarSoftwarePartition) => {
     return compiledData.map(
         (o): Software => ({
-            "serviceProviders": [], //todo use correct data
+            "serviceProviders": o.serviceProviders,
             "logoUrl": o.logoUrl ?? o.wikidataSoftware?.logoUrl ?? o.comptoirDuLibreSoftware?.logoUrl,
             "softwareId": o.id,
             "softwareName": o.name,
