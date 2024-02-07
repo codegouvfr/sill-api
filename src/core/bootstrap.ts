90026714fdb2100cc0cbc40ba24b6909fad6f8c0
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2024-02-07 16:31:50 +0100

    reorganize import in bootstrap

--- src/core/bootstrap.ts
+++ src/core/bootstrap.ts
@@ -1,5 +1,21 @@
 import { createObjectThatThrowsIfAccessed, createCore, type GenericCore } from "redux-clean-architecture";
 import { usecases } from "./usecases";
+import { createGetSoftwareLatestVersion } from "./adapters/getSoftwareLatestVersion";
+import { createCompileData } from "./adapters/compileData";
+import { getWikidataSoftware } from "./adapters/getWikidataSoftware";
+import { getCnllPrestatairesSill } from "./adapters/getCnllPrestatairesSill";
+import { comptoirDuLibreApi } from "./adapters/comptoirDuLibreApi";
+import { createGitDbApi } from "./adapters/dbApi";
+import { createKeycloakUserApi } from "./adapters/userApi";
+import { getWikidataSoftwareOptions } from "./adapters/getWikidataSoftwareOptions";
+import { getServiceProviders } from "./adapters/getServiceProviders";
+import type { DbApi } from "./ports/DbApi";
+import type { UserApi } from "./ports/UserApi";
+import type { CompileData } from "./ports/CompileData";
+import type { GetSoftwareExternalDataOptions } from "./ports/GetSoftwareExternalDataOptions";
+import type { ComptoirDuLibreApi } from "./ports/ComptoirDuLibreApi";
+import type { GetSoftwareExternalData } from "./ports/GetSoftwareExternalData";
+import type { GetSoftwareLatestVersion } from "./ports/GetSoftwareLatestVersion";
 
 type ParamsOfBootstrapCore = {
     gitDbApiParams: import("./adapters/dbApi").GitDbApiParams;
@@ -11,13 +27,13 @@ type ParamsOfBootstrapCore = {
 
 export type Context = {
     paramsOfBootstrapCore: ParamsOfBootstrapCore;
-    dbApi: import("./ports/DbApi").DbApi;
-    userApi: import("./ports/UserApi").UserApi;
-    compileData: import("./ports/CompileData").CompileData;
-    getWikidataSoftwareOptions: import("./ports/GetWikidataSoftwareOptions").GetWikidataSoftwareOptions;
-    comptoirDuLibreApi: import("./ports/ComptoirDuLibreApi").ComptoirDuLibreApi;
-    getWikidataSoftware: import("./ports/GetSoftwareExternalData").GetSoftwareExternalData;
-    getSoftwareLatestVersion: import("./ports/GetSoftwareLatestVersion").GetSoftwareLatestVersion;
+    dbApi: DbApi;
+    userApi: UserApi;
+    compileData: CompileData;
+    getWikidataSoftwareOptions: GetSoftwareExternalDataOptions;
+    comptoirDuLibreApi: ComptoirDuLibreApi;
+    getWikidataSoftware: GetSoftwareExternalData;
+    getSoftwareLatestVersion: GetSoftwareLatestVersion;
 };
 
 export type Core = GenericCore<typeof usecases, Context>;
@@ -34,28 +50,6 @@ export async function bootstrapCore(params: ParamsOfBootstrapCore): Promise<{ co
         doPerPerformPeriodicalCompilation,
         doPerformCacheInitialization
     } = params;
-
-    const [
-        { createGetSoftwareLatestVersion },
-        { createCompileData },
-        { getWikidataSoftware },
-        { getCnllPrestatairesSill },
-        { comptoirDuLibreApi },
-        { createGitDbApi },
-        { createKeycloakUserApi },
-        { getWikidataSoftwareOptions },
-        { getServiceProviders }
-    ] = await Promise.all([
-        import("./adapters/getSoftwareLatestVersion"),
-        import("./adapters/compileData"),
-        import("./adapters/getWikidataSoftware"),
-        import("./adapters/getCnllPrestatairesSill"),
-        import("./adapters/comptoirDuLibreApi"),
-        import("./adapters/dbApi"),
-        import("./adapters/userApi"),
-        import("./adapters/getWikidataSoftwareOptions"),
-        import("./adapters/getServiceProviders")
-    ] as const);
 
     const { getSoftwareLatestVersion } = createGetSoftwareLatestVersion({
         githubPersonalAccessTokenForApiRateLimit
